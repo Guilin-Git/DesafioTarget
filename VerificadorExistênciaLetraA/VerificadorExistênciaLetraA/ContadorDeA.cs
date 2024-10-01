86f896c1d58cc9bede87cb9916a45f676a7f570c
Guilherme Braga <guibragamarinho@gmail.com>
2024-10-01 19:03:06 -0300

    Contador de letra a na string

---
+++ VerificadorExistênciaLetraA/VerificadorExistênciaLetraA/ContadorDeA.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerificadorExistênciaLetraA
+{
+    internal class ContadorDeA
+    {
+        public int Contador;
+        public string Frase;
+        public ContadorDeA()
+        {
+            
+        }
+        public int VerificadorDeString(string frase)
+        {
+            Contador = 0;
+
+            foreach (char c in frase)
+            {
+                if (c == 'a' || c == 'A')
+                {
+                    Contador++;
+                }
+            }
+            return Contador;
+        }
+
+        public override string ToString()
+        {
+            return "A letra 'a' aparece " + Contador + " vezes na string !";
+        }
+    }
+}
---
+++ VerificadorExistênciaLetraA/VerificadorExistênciaLetraA/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using VerificadorExistênciaLetraA;
+
+namespace program // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContadorDeA cont;
+            Console.Write("Digite sua String: ");
+            string frase = Console.ReadLine();
+
+            cont = new ContadorDeA();
+            cont.VerificadorDeString(frase);
+            Console.WriteLine();
+            Console.WriteLine(cont);
+
+        }
+    }
+}
