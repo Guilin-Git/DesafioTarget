49530acfc9296a1c545938bd5b8372cb0988411c
Guilherme Braga <guibragamarinho@gmail.com>
2024-10-01 18:33:22 -0300

    first commit

---
+++ Fibonacci/Fibonacci/Fibonacci.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CalculateFibonacci
+{
+    internal class Fibonacci
+    {
+        public int A { get; set; }
+        public int B { get; set; }
+
+        public Fibonacci(int n)
+        {
+            A = 0;
+            B = 1;
+        }
+
+        public void PertenceFibonacci(int n)
+        {
+            if (n == A || n == B)
+            {
+                Console.WriteLine("Número pertence à sequência de Fibonacci");
+                return; 
+            }
+
+            int c = A + B;
+            while (c <= n)
+            {
+                if (c == n)
+                {
+                    Console.WriteLine("Número pertence à sequência de Fibonacci");
+                    return; 
+                }
+
+                A = B;
+                B = c;
+                c = A + B;
+            }
+
+            Console.WriteLine("Número não pertence à sequência de Fibonacci");
+        }
+    }
+}
---
+++ Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using CalculateFibonacci;
+using System;
+using System.Threading.Channels;
+
+namespace program 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Olá, Deseja saber se um numero pertence a sequencia de Fibonacci? (s/n) ");
+            char validate = char.Parse(Console.ReadLine());
+            while (validate == 's')
+            {
+                Console.Write("Insira um valor inteiro para ver se ele pertence a sequência de Fibonacci: ");
+                int n = int.Parse(Console.ReadLine());
+
+                Fibonacci fib = new Fibonacci(n);
+                Console.WriteLine();
+                fib.PertenceFibonacci(n);
+                Console.WriteLine();
+                Console.Write("Deseja continuar? (s/n) ");
+                validate = char.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Obrigado por usar o programa!!!");
+        }
+
+    }
+}
